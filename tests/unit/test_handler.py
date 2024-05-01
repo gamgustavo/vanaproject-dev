c7daf82bded6b5863647e749752ed7bf0d188a66
gamgustavo <holagamboatavo@gmail.com>
2024-04-30 22:40:04 -0600

    Fix bad output unit test
    Fix the error:
    
    FAILED tests/unit/test_handler.py::test_lambda_handler - assert 'message' in '"\\u00a1Hola desde AWS Lambda! Mi nombre es..."'

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -69,4 +69,4 @@ def test_lambda_handler(apigw_event):
 
     assert ret["statusCode"] == 200
     assert "message" in ret["body"]
-    assert data["message"] == "\"\\u00a1Hola desde AWS Lambda! Mi nombre es...\""
+    assert "Hola desde AWS Lambda! Mi nombre es" in data["message"]
